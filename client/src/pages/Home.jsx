caacce6517007b716cffeacb69f2bc156de28d98
Shubham Saurav <ssaurav28502@gmail.com>
2024-06-30 12:50:16 +0530

    home complete

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HomeBanner from '../components/HomeBanner'
+import HomeContent from '../components/HomeContent'
 
 
 export default function Home() {
   return (
     <>
       <HomeBanner/>
+      <HomeContent/>
     </>
   )
 }
