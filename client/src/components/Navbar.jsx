67acafb7edc009920922c37610e6b3a7138a11c3
Shubham Saurav <ssaurav28502@gmail.com>
2024-07-02 13:23:39 +0530

    scrolling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,12 +14,13 @@ export default function Navbar() {
   return (
     <div className=''>
       <nav className=' flex justify-between shadow-lg py-2 w-full bg-white fixed top-0 z-50'>
+        <NavLink to="/">
         <div onMouseEnter={mousehover} onMouseLeave={mousenothover} className='ml-7 flex items-center gap-3'>
             {isHover?<img id='logoimg' src={Logo} alt="JobDekho" className='h-[60px]'/>:<img id='logoimg' src={LogoStatic} alt="JobDekho" className='h-[60px]'/>}
             
-            <p  className='text-2xl font-semibold'><span className='text-red-400'>Job</span><span className='text-yellow-500'>Dekho</span></p>
-           
+            <p  className='text-2xl font-semibold'><span className='text-red-400'>Job</span><span className='text-yellow-500'>Dekho</span></p> 
         </div>
+        </NavLink>
         <div className='flex w-[50%] justify-center'>
             <ul className='flex items-center gap-7'>
             <li className='hover:text-purple-500 font-semibold'>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import HomeBanner from '../components/HomeBanner'
 import HomeContent from '../components/HomeContent'
 
 
 export default function Home() {
+  useEffect(()=>{
+    window.scrollTo(0,0);
+  },[])
+  
   return (
     <>
       <HomeBanner/>
